e0aaa4d05b470f8f7d0ad54ece582386b510aa3c
Nemanja Planic <planicnemanja@gmail.com>
2024-12-20 16:23:21 +0100

    Dorada stranice za kupovinu

--- karte/urls.py
+++ karte/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.kupovinaForma, name='buypage'),
-    path('uspesna-kupovina',views.uspesna_kupovina, name='uspesna_kupovina')
+    path('uspesna-kupovina',views.uspesna_kupovina, name='uspesna_kupovina'),
+    path('greska', views.greska, name='greska')
 ]
--- karte/views.py
+++ karte/views.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from django.shortcuts import get_object_or_404, redirect, render
 from karte.form import FormaKupovina
 from karte.models import PreostaloKarata
@@ -8,13 +9,24 @@ def kupovinaForma(zahtev):
     if zahtev.method == 'POST':
         forma = FormaKupovina(zahtev.POST)
         if forma.is_valid():
-            return redirect('uspesna_kupovina')
+            tipkarte = zahtev.POST.get('tip_karte')
+            kolicina = int(zahtev.POST.get('broj_karata'))
+            izbor = get_object_or_404(PreostaloKarata, utakmica=get_object_or_404(Utakmica, id=zahtev.GET.get('id')), tip_karte=tipkarte)
+            if izbor.preostalo >= kolicina:
+                izbor.preostalo -= kolicina
+                izbor.save()
+                return redirect('uspesna_kupovina')
+            else:
+                return redirect('greska')
     else:
         utakmica_id = zahtev.GET.get('id')  # Uzimanje ID-a iz GET parametra
         utakm = get_object_or_404(Utakmica, id=utakmica_id)
         forma = FormaKupovina()
         ostalo = PreostaloKarata.objects.filter(utakmica=utakm)
-    return render(zahtev,'kupovina.html',{'forma':forma,'ostalo':ostalo})
+    return render(zahtev,'kupovina.html',{'forma':forma,'ostalo':ostalo,'tekma':utakm})
 
 def uspesna_kupovina(zahtev):
     return render(zahtev,'uspesna-kupovina.html')
+
+def greska(zahtev):
+    return render(zahtev,'greskapage.html')
